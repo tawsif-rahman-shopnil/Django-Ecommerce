32cdf02a2fa904b1ece932464170c211cf4173f0
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-07-31 00:00:54 +0600

    update

--- demo/settings.py
+++ demo/settings.py
@@ -1,7 +1,7 @@
 import os
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
